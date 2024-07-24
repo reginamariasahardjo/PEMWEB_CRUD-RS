05931548ec4a9996cc5d0bd856f8c8a7d47e6639
christyaurelia9@gmail.com <christyaurelia9@gmail.com>
2024-07-24 14:35:25 +0700

    Tugas terakhir CRUD

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <div class="container">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a href="#" class="navbar-brand btn-custom">My App</a>
+            <a href="#" class="navbar-brand btn-custom">yang ini versi aurel</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
